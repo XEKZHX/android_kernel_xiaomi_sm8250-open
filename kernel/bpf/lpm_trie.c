774ab4e875cd34d7c3018ddeb6d49a2b344d79d5
AGXMX <85810478+XEKZHX@users.noreply.github.com>
2025-06-17 20:59:31 +0800

    Update lpm_trie.c for bpf

--- kernel/bpf/lpm_trie.c
+++ kernel/bpf/lpm_trie.c
@@ -695,8 +695,9 @@ free_stack:
 }
 
 static int trie_check_btf(const struct bpf_map *map,
-			  const struct btf_type *key_type,
-			  const struct btf_type *value_type)
+			const struct btf *btf,
+			const struct btf_type *key_type,
+			const struct btf_type *value_type)
 {
 	/* Keys must have struct bpf_lpm_trie_key embedded. */
 	return BTF_INFO_KIND(key_type->info) != BTF_KIND_STRUCT ?

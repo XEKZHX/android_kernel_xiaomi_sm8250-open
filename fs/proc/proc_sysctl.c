3f0608221e9f369d9c989c20622f9ed0a5f905f7
AGXMX <85810478+XEKZHX@users.noreply.github.com>
2025-06-17 12:53:27 +0800

    Update proc_sysctl.c for bpf

--- fs/proc/proc_sysctl.c
+++ fs/proc/proc_sysctl.c
@@ -13,6 +13,7 @@
 #include <linux/namei.h>
 #include <linux/mm.h>
 #include <linux/module.h>
+#include <linux/bpf-cgroup.h>
 #include "internal.h"
 
 static const struct dentry_operations proc_sys_dentry_operations;
@@ -591,7 +592,11 @@ static ssize_t proc_sys_call_handler(struct file *filp, void __user *buf,
 	error = -EINVAL;
 	if (!table->proc_handler)
 		goto out;
-
+	
+	error = BPF_CGROUP_RUN_PROG_SYSCTL(head, table, write);
+	if (error)
+		goto out;
+	
 	/* careful: calling conventions are nasty here */
 	res = count;
 	error = table->proc_handler(table, write, buf, &res, ppos);

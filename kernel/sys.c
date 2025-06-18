c7284be05f290cffe350f6cae23f4120e3361c68
AGXMX <85810478+XEKZHX@users.noreply.github.com>
2025-06-18 15:17:45 +0800

    Update sys.c for bpf

--- kernel/sys.c
+++ kernel/sys.c
@@ -1253,6 +1253,13 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 		pr_debug("fake uname: %s release=%s\n",
 			 current->comm, tmp.release);
 	}
+	if (!strncmp(current->comm, "bpfloader", 9) ||
+	    !strncmp(current->comm, "netbpfload", 10) ||
+	    !strncmp(current->comm, "netd", 4)) {
+		strcpy(tmp.release, "5.4.186");
+		pr_debug("fake uname: %s/%d release=%s\n",
+			 current->comm, current->pid, tmp.release);
+	}
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
